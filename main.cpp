68d713f082d840e83702cc7aec706e77c047d9d7
shubhampawar672 <80095959+shubhampawar672@users.noreply.github.com>
2021-12-06 20:45:27 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int top=-1;
+char Stack[50];
+void push(char x){
+
+if(top==49)
+   {
+
+    cout<<"stack overflow";
+    return;}
+else
+    Stack[++top]=x;
+}
+void pop(){
+if(top==-1){
+    cout<<"stack underflow";
+    return;
+}
+cout<<Stack[top--];
+}
+int main()
+{
+    char str[10];
+    for(int j=0;j<10;j++){
+        cin>>str[j];
+    }
+    int len=strlen(str),i;
+    for(i=0;i<len;i++){
+
+        push(str[i]);
+    }
+     for(i=0;i<len;i++){
+        pop();}
+        return 0;
+
+}
